8665a62e7bcfb9a6cddae5251c9ceb2d2eab636f
Abdulaziz-Ahmed44 <abdulazizahmed447@gmail.com>
2025-08-30 18:35:21 +0300

    Session

---
+++ Calc_Extension_Method/Calc_Extension_Method/Extensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_Extension_Method
+{
+     public static class Extensions
+    {
+        public static T Sum<T>(this IEnumerable<T> items) where T : INumber<T> 
+        {
+            T total = T.Zero;
+            foreach (var item in items) 
+            {
+                total += item;
+            }
+            return total;
+        }
+
+        public static T Subtract<T>(this IEnumerable<T> items) where T : INumber<T>
+        {
+            T total = T.Zero;
+            foreach (var item in items)
+            {
+                total = item - total;
+            }
+            
+            return total;
+        }
+
+        public static T Multiplicate<T>(this IEnumerable<T> items) where T : INumber<T>
+        {
+            T total = T.One;
+            foreach (var item in items)
+            {
+                total *= item;
+            }
+            return total;
+        }
+
+        public static T Division<T>(this IEnumerable<T> items) where T : INumber<T>
+        {
+            T total = T.One;
+            foreach (var item in items)
+            {
+                total = item / total;
+            }
+            return total;
+        }
+
+        public static T Modulo<T>(this IEnumerable<T> items) where T : INumber<T>
+        {
+            T total = T.One;
+            foreach (var item in items)
+            {
+                total %= item ;
+            }
+            return total;
+        }
+    }
+
+}
---
+++ Calc_Extension_Method/Calc_Extension_Method/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Calc_Extension_Method
+{
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>() {10,20,30,40 };
+            var numSum = Numbers.Sum();
+            Console.WriteLine($"Sum of numbers: {Numbers.Sum()}");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Subtraction of numbers: {Numbers.Subtract()}");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Multiplication of numbers: {Numbers.Multiplicate()}");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Division of numbers: {Numbers.Division()}");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Modulas of numbers: {Numbers.Modulo()}");
+            Console.WriteLine("---------------------------------------");
+
+
+
+        }
+    }
+}
